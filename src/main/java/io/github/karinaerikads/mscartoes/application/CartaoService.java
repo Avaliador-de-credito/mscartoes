35a7accf6ca6e0653e3be0da243c86716a4cb172
karinaerikads <karinaerikads48@gmail.com>
2024-02-09 01:57:11 -0300

    feat: adiciona método que encontra cartão com renda menor ou igual a repassada no service

---
+++ src/main/java/io/github/karinaerikads/mscartoes/application/CartaoService.java
@@ -0,0 +1,27 @@
+package io.github.karinaerikads.mscartoes.application;
+
+import io.github.karinaerikads.mscartoes.domain.Cartao;
+import io.github.karinaerikads.mscartoes.infra.repository.CartaoRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CartaoService {
+
+    private final CartaoRepository cartaoRepository;
+
+    @Transactional
+    public Cartao save (Cartao cartao){
+        return cartaoRepository.save();
+    }
+
+    public List<Cartao> getCartoesRendaMenosIgual(Long renda){
+        var rendaBigDecimal = BigDecimal.valueOf(renda);
+        return cartaoRepository.findByRendaLessThanEqual(rendaBigDecimal);
+    }
+}
--- src/main/java/io/github/karinaerikads/mscartoes/infra/repository/CartaoRepository.java
+++ src/main/java/io/github/karinaerikads/mscartoes/infra/repository/CartaoRepository.java
@@ -3,5 +3,9 @@ package io.github.karinaerikads.mscartoes.infra.repository;
 import io.github.karinaerikads.mscartoes.domain.Cartao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface CartaoRepository extends JpaRepository<Cartao, Long> {
+    List<Cartao> findByRendaLessThanEqual(BigDecimal renda);
 }
