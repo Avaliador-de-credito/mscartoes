66cad42fa263e7d4ca7ea5c3f7020012d04d6a0b
karinaerikads <karinaerikads48@gmail.com>
2024-02-09 14:58:18 -0300

    feat: adiciona propriedade cpf

--- src/main/java/io/github/karinaerikads/mscartoes/domain/ClienteCartao.java
+++ src/main/java/io/github/karinaerikads/mscartoes/domain/ClienteCartao.java
@@ -14,7 +14,6 @@ public class ClienteCartao {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private  String cpf;
     @ManyToOne
     @JoinColumn(name = "id_cartao")
     private Cartao cartao;
